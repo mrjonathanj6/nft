2feaf32180b3073c8dfa8c2211e770b68176de07
jonathan <mrjonathanj6@gmail.com>
2023-09-26 11:58:49 +0330

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# nft
+
+![alt text](https://github.com/mrjonathanj6/nft/blob/main/asset/pic/nft-en.jpg)
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html"
+    // './components/**/*.{html,js}', باید فایل js برای منو اضافه بشه دنبال اون بگرد تا اضافش کنی به اینجا یا همچین چیزی
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
